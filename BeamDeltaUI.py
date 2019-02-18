0a0aeb0b55cf443138e6ed30e1439e844b51ae1b
NickHallONBI <nicholas.hall@dtc.ox.ac.uk>
2019-02-18 17:57:17 +0000

    Text display x and y distances

--- BeamDeltaUI.py
+++ BeamDeltaUI.py
@@ -36,6 +36,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.form_widget)
         self.resize(self.form_widget.width()+100, self.form_widget.height()+100)
         self.show_flag = True
+        self.setWindowTitle("Beam Delta")
 
     def closeEvent(self, event):
         self.show_flag = False
@@ -75,6 +76,9 @@ class CamInterfaceApp(QWidget):
         self.x_cur_cent = None
         self.y_cur_cent = None
 
+        self.diff_x = None
+        self.diff_y = None
+
         self.imager = None
         self.colimage = None
         self.setImager(imager)
@@ -84,10 +88,13 @@ class CamInterfaceApp(QWidget):
         self.buttons.live_button.clicked.connect(self.toggleLiveImage)
         self.buttons.align_cent_button.clicked.connect(self.toggleAlignCent)
         self.buttons.curr_cent_button.clicked.connect(self.toggleCurrCent)
+        self.text = QLabel(self)
+        self.text.setAlignment(Qt.AlignHCenter)
 
         layout = QVBoxLayout()
         layout.addWidget(self.camera)
         layout.addWidget(self.buttons)
+        layout.addWidget(self.text)
         self.setLayout(layout)
 
         self.app_width = min(self.camera.pixmap.width(),self.buttons.width())
@@ -135,6 +142,7 @@ class CamInterfaceApp(QWidget):
                 self.calcCurCentroid(data)
                 curr_y = int(np.round(self.x_cur_cent))
                 curr_x = int(np.round(self.x_cur_cent))
+                self.arm_length = int(self.colimage.shape[1] * 0.05)
 
                 self.colimage[curr_y - self.arm_length:curr_y + self.arm_length, curr_x, 1] = np.max(data)
                 self.colimage[curr_y, curr_x - self.arm_length:curr_x + self.arm_length, 1] = np.max(data)
@@ -179,6 +187,7 @@ class CamInterfaceApp(QWidget):
                 self.calcCurCentroid(self.colimage[:, :, 0])
                 curr_y = int(np.round(self.x_cur_cent))
                 curr_x = int(np.round(self.x_cur_cent))
+                self.arm_length = int(self.colimage.shape[1] * 0.05)
 
                 paused_image[curr_y - self.arm_length:curr_y + self.arm_length, curr_x, 1] = np.max(
                     paused_image[:,:,2])
@@ -191,6 +200,11 @@ class CamInterfaceApp(QWidget):
             self.camera.pixmap = QPixmap(qimage)
             self.camera.label.setPixmap(self.camera.pixmap)
 
+        if self.diff_y is None:
+            self.text.setText("X distance = N/A, Y distance = N/A")
+        else:
+            self.text.setText("X distance = %f, Y distance = %f" % (self.diff_x, self.diff_y))
+
     def calcCurCentroid(self, image):
         thresh = threshold_otsu(image)
         binaryIm = image > thresh
@@ -199,7 +213,8 @@ class CamInterfaceApp(QWidget):
         self.y_cur_cent, self.x_cur_cent = center_of_mass(imageOtsu[10:-10, 10:-10])
 
         if self.y_alig_cent == None or self.x_alig_cent == None:
-            pass
+            self.diff_x = None
+            self.diff_y = None
         else:
             self.diff_y = self.y_cur_cent - self.y_alig_cent
             self.diff_x = self.x_cur_cent - self.x_alig_cent
@@ -213,10 +228,14 @@ class CamInterfaceApp(QWidget):
     def toggleAlignCent(self):
         self.x_alig_cent = None
         self.y_alig_cent = None
+        self.diff_x = None
+        self.diff_y = None
         self.align_cent_flag = not(self.align_cent_flag)
 
     @pyqtSlot()
     def toggleCurrCent(self):
+        self.diff_x = None
+        self.diff_y = None
         self.curr_cent_flag = not (self.curr_cent_flag)
 
 class ToggleButtonApp(QWidget):
