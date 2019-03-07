1f4029bf11ed4e2f073c2f3e589e91cb330e0e8d
Carnë Draug <carandraug+dev@gmail.com>
2019-03-07 14:32:49 +0000

    maint: fix typo on copyright header

--- BeamDeltaUI.py
+++ BeamDeltaUI.py
@@ -16,7 +16,7 @@
 ## GNU General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
-## along with BeamDeltaCOPYING.  If not, see <http://www.gnu.org/licenses/>.
+## along with BeamDelta.  If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import sys
