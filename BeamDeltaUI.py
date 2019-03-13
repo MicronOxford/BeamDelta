b635633a682d1f66e6bcdcc3e1b67c502555dd63
Carnë Draug <carandraug+dev@gmail.com>
2019-03-13 17:49:38 +0000

    Split CamInterfaceApp in a Model-View pattern, and use QPainter to draw cross
    This adds the automatic resizing of the view.  And removes the turn
    on/off view of the alignment centroid just because.  Also changes for
    just a button to update reference.

--- BeamDeltaUI.py
+++ BeamDeltaUI.py
@@ -22,24 +22,40 @@ import argparse
 import sys
 
 from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QMainWindow,
-                             QCheckBox, QVBoxLayout, QWidget)
-from PyQt5.QtGui import QImage, QPainter, QPixmap
-from PyQt5.QtCore import QObject, QSize, QTimer, Qt, pyqtSignal, pyqtSlot
+                             QCheckBox, QPushButton, QVBoxLayout, QWidget)
 
-from microscope import clients
+from PyQt5.QtGui import QImage, QPainter, QPen
+
+from PyQt5.QtCore import(QObject, QPoint, QSize, QTimer, Qt,
+                         pyqtSignal, pyqtSlot)
+
 import numpy as np
 from skimage.filters import threshold_otsu
 from scipy.ndimage.measurements import center_of_mass
 
+import microscope.clients
+
 
 class Imager(QObject):
+    """Live acquisition imager.
 
-    imageReceived = pyqtSignal(np.ndarray)
+    Starts acquiring images once enabled and sends an
+    ``imageAcquired`` signal.
+
+    Args:
+        uri (str): URI for microscope device.
+        exposure (float): time in seconds.
+    """
+
+    imageAcquired = pyqtSignal(np.ndarray)
 
     def __init__(self, uri, exposure):
         super().__init__()
-        self._client = clients.DataClient(uri)
+        self._client = microscope.clients.DataClient(uri)
         self._client.set_exposure_time(exposure)
+
+        self._image = np.zeros(self.shape(), dtype=np.uint8)
+
         self._timer = QTimer(self)
         self._timer.timeout.connect(self._acquire)
 
@@ -55,183 +71,50 @@ class Imager(QObject):
         """Shape of images (width, height)"""
         return self._client.get_sensor_shape()
 
+    def image(self):
+        return self._image
+
     def _acquire(self):
-        image = self._client.trigger_and_wait()[0]
-        self.imageReceived.emit(image)
+        self._image = self._client.trigger_and_wait()[0]
+        self.imageAcquired.emit(self._image)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self, imager1, imager2, parent=None):
-        super().__init__(parent)
-        self.form_widget = MainWidget(self, imager1, imager2)
-        self.setCentralWidget(self.form_widget)
+class Alignment(QObject):
+    """Model for an alignment.
+    """
+    changed = pyqtSignal()
 
-
-class MainWidget(QWidget):
-    def __init__(self, parent, imager1, imager2):
-        super().__init__(parent)
-        self.camera1 = CamInterfaceApp(self, imager1)
-        self.camera2 = CamInterfaceApp(self, imager2)
-
-        layout = QHBoxLayout(self)
-        layout.addWidget(self.camera1)
-        layout.addWidget(self.camera2)
-        self.setLayout(layout)
-
-
-class CamInterfaceApp(QWidget):
-    def __init__(self, parent, imager):
-        super().__init__(parent)
-        self.x_align_cent = None
-        self.y_align_cent = None
-
-        self.x_cur_cent = None
-        self.y_cur_cent = None
-
-        self.diff_x = None
-        self.diff_y = None
-
-        shape = imager.shape()
-        self.colimage = np.zeros((shape[0], shape[1], 3), dtype=np.uint8)
-
-        self.imager = imager
-        self.imager.imageReceived.connect(self.updateView)
-
-        self.view = CameraView(self.imager)
-
-        self.live_button = QCheckBox("Live")
-        self.live_button.stateChanged.connect(self.changeLiveMode)
-        self.live_button.setChecked(True) # default to Live
-        ## These are a bit misleading, because they don't control only
-        ## show/hide, they may also reset a previous position.
-        self.align_cent_button = QCheckBox("Show Reference")
-        self.curr_cent_button = QCheckBox("Show Current")
-
-        self.align_cent_button.stateChanged.connect(self.toggleAlignCent)
-        self.curr_cent_button.stateChanged.connect(self.toggleCurrCent)
-        self.text = QLabel(self)
-        self.text.setAlignment(Qt.AlignHCenter)
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.view, 1)
-
-        buttons = QHBoxLayout()
-        buttons.addWidget(self.live_button)
-        buttons.addWidget(self.align_cent_button)
-        buttons.addWidget(self.curr_cent_button)
-        layout.addLayout(buttons)
-
-        layout.addWidget(self.text)
-
-        self.setLayout(layout)
-
-    def updateView(self, image):
-        if self.live_button.isChecked():
-            #Collect live image
-            self.colimage[:, :, 0] = np.array(image)
-            self.colimage[:, :, 1] = self.colimage[:, :, 0]
-            self.colimage[:, :, 2] = self.colimage[:, :, 0]
-
-            self.updateAlignmentCentroid(self.colimage)
-            self.updateCurrentCentroid(self.colimage)
-            self.setCurrentImage(self.colimage)
-
-            # Reset values overwritten by crosshairs
-            self.colimage[:, :, 0] = np.array(image)
-            self.colimage[:, :, 1] = self.colimage[:, :, 0]
-            self.colimage[:, :, 2] = self.colimage[:, :, 0]
-        else:
-            # Copy last collected image as current frame
-            paused_image = self.colimage.copy()
-            self.updateAlignmentCentroid(paused_image)
-            self.updateCurrentCentroid(paused_image)
-            self.setCurrentImage(paused_image)
-
-        if self.diff_y is None:
-            self.text.setText("X distance = N/A, Y distance = N/A")
-        else:
-            self.text.setText("X distance = %f, Y distance = %f" % (self.diff_x, self.diff_y))
-
-    def updateAlignmentCentroid(self, image):
-        """Check if should be shown and update alignment centroid"""
-        if not self.align_cent_button.isChecked():
-            return
-
-        if self.x_align_cent is None:
-            ## Calculate position of alignment centroid if not yet done
-            self.calcCurCentroid(image[:, :, 0])
-            self.x_align_cent = self.x_cur_cent
-            self.y_align_cent = self.y_cur_cent
-
-        draw_crosshairs(image, self.x_align_cent, self.y_align_cent, 0)
-
-    def updateCurrentCentroid(self, image):
-        """Check if should be shown and update current centroid"""
-        if not self.curr_cent_button.isChecked():
-            return
-
-        self.calcCurCentroid(image[:, :, 0])
-        draw_crosshairs(image, self.x_cur_cent, self.y_cur_cent, 1)
-
-    def setCurrentImage(self, image):
-        """Set current image (with any crosshairs) as current frame"""
-        self.view.updateImage(image)
-
-    def calcCurCentroid(self, image):
-        self.y_cur_cent, self.x_cur_cent = compute_beam_centre(image)
-        if self.y_align_cent is None or self.x_align_cent is None:
-            self.diff_x = None
-            self.diff_y = None
-        else:
-            self.diff_y = self.y_cur_cent - self.y_align_cent
-            self.diff_x = self.x_cur_cent - self.x_align_cent
-
-    @pyqtSlot(int)
-    def changeLiveMode(self, state):
-        if state == Qt.Checked:
-            self.imager.enable()
-        else:
-            self.imager.disable()
-
-    @pyqtSlot(int)
-    def toggleAlignCent(self, state):
-        if state != Qt.Checked:
-            self.x_align_cent = None
-            self.y_align_cent = None
-            self.diff_x = None
-            self.diff_y = None
-
-    @pyqtSlot(int)
-    def toggleCurrCent(self, state):
-        if state != Qt.Checked:
-            self.diff_x = None
-            self.diff_y = None
-
-
-class CameraView(QWidget):
     def __init__(self, imager):
         super().__init__()
         self._imager = imager
-        shape = self._imager.shape()
-        self._image = None # FIXME: we should not have to deal with Nones
-        self._setImage(np.zeros((shape[0], shape[1], 3), dtype=np.uint8))
+        self._image = self._imager.image()
+        self._current = compute_beam_centre(self._image)
+        self._reference = self._current
 
-    def _setImage(self, image):
-        ## XXX: I really don't like this
-        self._image = QPixmap(QImage(image.tobytes(),
-                                     image.shape[1], image.shape[0],
-                                     QImage.Format_RGB888))
+        self._imager.imageAcquired.connect(self.setCurrentImage)
 
-    def sizeHint(self):
-        return QSize(*self._imager.shape())
+    def reference(self):
+        return self._reference
 
-    def updateImage(self, image):
-        self._setImage(image)
-        self.update()
+    def current(self):
+        return self._current
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawPixmap(self.rect(), self._image)
+    def image(self):
+        return self._image
+
+    @pyqtSlot(np.ndarray)
+    def setCurrentImage(self, image):
+        self._image = image
+        self._current = compute_beam_centre(self._image)
+        self.changed.emit()
+
+    @pyqtSlot()
+    def updateReference(self):
+        self._reference = compute_beam_centre(self._imager.image())
+        self.changed.emit()
+
+    def offset(self):
+        return tuple([c-r for c, r in zip(self._current, self._reference)])
 
 
 def compute_beam_centre(image):
@@ -240,24 +123,132 @@ def compute_beam_centre(image):
         tuple with coordinates for centre ordered by dimension,
         i.e. (y, x)
     """
+    try:
+        thresh = threshold_otsu(image)
+    except ValueError:
+        ## Happens for example if all pixels in the image have the
+        ## same value.  Return the middle of the image.
+        return [l/2 for l in image.shape]
+
     ## TODO: find out why we cut the 10px edges, and either comment
     ## here why it is done, or remove the removal of edges.
     edge_len = 10
     masked = image[edge_len:-edge_len, edge_len:-edge_len].copy()
-    masked[masked < threshold_otsu(image)] = 0
-    centre = center_of_mass(masked)
-    centre = [c+edge_len for c in centre]
-    return centre
+    masked[masked < thresh] = 0
+    return [c+edge_len for c in center_of_mass(masked)]
 
 
-def draw_crosshairs(image, x, y, channel):
-    x = int(np.round(x))
-    y = int(np.round(y))
-    length = int(image.shape[1] * 0.05)
+class MainWindow(QMainWindow):
+    def __init__(self, imager1, imager2, parent=None):
+        super().__init__(parent)
+        self.setCentralWidget(CentralWidget(self, imager1, imager2))
 
-    val = np.iinfo(image.dtype).max
-    image[y-length : y+length, x, channel] = val
-    image[y, x-length : x+length, channel] = val
+
+class CentralWidget(QWidget):
+    def __init__(self, parent, imager1, imager2):
+        super().__init__(parent)
+        self.camera1 = AlignmentControl(imager1)
+        self.camera2 = AlignmentControl(imager2)
+
+        layout = QHBoxLayout(self)
+        layout.addWidget(self.camera1)
+        layout.addWidget(self.camera2)
+        self.setLayout(layout)
+
+
+class AlignmentControl(QWidget):
+    def __init__(self, imager):
+        super().__init__()
+        self._imager = imager
+        self._alignment = Alignment(imager)
+        self._visual = AlignmentVisual(self._alignment)
+        self._text = AlignmentText(self._alignment)
+
+        self._update_button = QPushButton('Update Reference')
+        self._update_button.clicked.connect(self._alignment.updateReference)
+
+        self._live_checkbox = QCheckBox("Live")
+        self._live_checkbox.stateChanged.connect(self.changeLiveMode)
+
+        self._live_checkbox.setChecked(True)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self._visual, 1)
+
+        buttons = QHBoxLayout()
+        buttons.addWidget(self._update_button)
+        buttons.addWidget(self._live_checkbox)
+        layout.addLayout(buttons)
+
+        layout.addWidget(self._text)
+        self.setLayout(layout)
+
+    @pyqtSlot(int)
+    def changeLiveMode(self, state):
+        if state == Qt.Checked:
+            self._imager.enable()
+        else:
+            self._imager.disable()
+
+
+class AlignmentText(QLabel):
+    """Text "view" for Alignment"""
+    def __init__(self, alignment):
+        super().__init__()
+        self.setAlignment(Qt.AlignHCenter)
+
+        self._alignment = alignment
+        self._alignment.changed.connect(self.updateText)
+
+        self.updateText()
+
+    @pyqtSlot()
+    def updateText(self):
+        self.setText("X distance = %f, Y distance = %f"
+                     % self._alignment.offset())
+
+
+class AlignmentVisual(QWidget):
+    """Visual view for Alignment"""
+    def __init__(self, alignment):
+        super().__init__()
+        self._alignment = alignment
+        self._alignment.changed.connect(self.updateView)
+
+    def sizeHint(self):
+        return QSize(*self._alignment.image().shape)
+
+    @pyqtSlot()
+    def updateView(self):
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+
+        image = self._alignment.image()
+        painter.drawImage(self.rect(), QImage(image.tobytes(), *image.shape,
+                                              QImage.Format_Grayscale8))
+
+        painter.setCompositionMode(QPainter.CompositionMode_Lighten)
+        pen = QPen()
+        pen.setWidth(self.width()/500)
+        pen.setCapStyle(Qt.RoundCap)
+
+        painter.scale(self.width() / image.shape[0],
+                      self.height() / image.shape[1])
+
+        def drawCentre(color, x, y):
+            pen.setColor(color)
+            painter.setPen(pen)
+            length = self.width() /50
+            painter.translate(x, y)
+            painter.drawLine(-length, -length, length, length)
+            painter.drawLine(-length, length, length, -length)
+            painter.drawEllipse(QPoint(0, 0), length/2, length/2)
+            painter.translate(-x, -y)
+
+        drawCentre(Qt.red, *self._alignment.reference())
+        drawCentre(Qt.green, *self._alignment.current())
 
 
 def parse_arguments(arguments):
